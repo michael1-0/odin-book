748e07c79d988266fe6c5b43b6947813aaeb6662
Michael Bagaskoro Edwards <143170516+michael1-0@users.noreply.github.com>
2026-07-13 13:36:43 +0700

    Refactor zod package for readability

--- packages/zod-validations/src/index.ts
+++ packages/zod-validations/src/index.ts
@@ -1,89 +1,44 @@
-import z from "zod";
-import {
-  UserGetParamsSchema,
-  UserGetQuerySchema,
-  UserUpdateBodySchema,
-  UserUpdateParamsSchema,
-  UserWithFollowStatusSchema,
-  UserWithPostsSchema,
-} from "./users.js";
-import { FollowParamsSchema } from "./follows.js";
-import { LikeFeedSchema } from "./likes.js";
-import { CommentCreateBodySchema } from "./comments.js";
-import {
-  PostCreateSchema,
-  PostFeedItemSchema,
-  PostFeedItemWithCommentsSchema,
-  PostGetParamsSchema,
-  PostsGetQuerySchema,
-  PostGetQuerySchema,
-  PostLikeParamsSchema,
-} from "./posts.js";
+export { z } from "zod";
 
 // Users
-type UserWithFollowStatus = z.infer<typeof UserWithFollowStatusSchema>;
-type UserUpdateBody = z.infer<typeof UserUpdateBodySchema>;
-type UserUpdateParams = z.infer<typeof UserUpdateParamsSchema>;
-type UserGetParams = z.infer<typeof UserGetParamsSchema>;
-type UserGetQuery = z.infer<typeof UserGetQuerySchema>;
-type UserWithPosts = z.infer<typeof UserWithPostsSchema>;
 export {
-  UserWithFollowStatusSchema,
   UserUpdateBodySchema,
   UserUpdateParamsSchema,
   UserGetParamsSchema,
   UserGetQuerySchema,
+  UserWithFollowStatusSchema,
   UserWithPostsSchema,
-};
-export type {
-  UserWithFollowStatus,
-  UserUpdateBody,
-  UserUpdateParams,
-  UserGetParams,
-  UserGetQuery,
-  UserWithPosts,
-};
-
-// Follows
-type FollowParams = z.infer<typeof FollowParamsSchema>;
-export { FollowParamsSchema };
-export type { FollowParams };
-
-// Likes
-type LikeFeed = z.infer<typeof LikeFeedSchema>;
-export { LikeFeedSchema };
-export type { LikeFeed };
-
-// Comments
-type CommentCreateBody = z.infer<typeof CommentCreateBodySchema>;
-export { CommentCreateBodySchema };
-export type { CommentCreateBody };
+  type UserUpdateBody,
+  type UserUpdateParams,
+  type UserGetParams,
+  type UserGetQuery,
+  type UserWithFollowStatus,
+  type UserWithPosts,
+} from "./users.js";
 
 // Posts
-type PostFeedItem = z.infer<typeof PostFeedItemSchema>;
-type PostFeedItemWithComments = z.infer<typeof PostFeedItemWithCommentsSchema>;
-type PostCreate = z.infer<typeof PostCreateSchema>;
-type PostLikeParams = z.infer<typeof PostLikeParamsSchema>;
-type PostGetParams = z.infer<typeof PostGetParamsSchema>;
-type PostsGetQuery = z.infer<typeof PostsGetQuerySchema>;
-type PostGetQuery = z.infer<typeof PostGetQuerySchema>;
 export {
-  PostFeedItemSchema,
-  PostFeedItemWithCommentsSchema,
   PostCreateSchema,
   PostLikeParamsSchema,
   PostGetParamsSchema,
   PostsGetQuerySchema,
   PostGetQuerySchema,
-};
-export type {
-  PostFeedItem,
-  PostFeedItemWithComments,
-  PostCreate,
-  PostLikeParams,
-  PostGetParams,
-  PostsGetQuery,
-  PostGetQuery,
-};
+  PostFeedItemSchema,
+  PostFeedItemWithCommentsSchema,
+  type PostCreate,
+  type PostLikeParams,
+  type PostGetParams,
+  type PostsGetQuery,
+  type PostGetQuery,
+  type PostFeedItem,
+  type PostFeedItemWithComments,
+} from "./posts.js";
 
-export default z;
+// Comments
+export { CommentCreateBodySchema, type CommentCreateBody } from "./comments.js";
+
+// Follows
+export { FollowParamsSchema, type FollowParams } from "./follows.js";
+
+// Likes
+export { LikeFeedSchema, type LikeFeed } from "./likes.js";
